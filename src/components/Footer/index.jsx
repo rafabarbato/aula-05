b3e9bd9bb311170372bb0a108aa7abb9e55f92ef
rafabarbato <kadeshbrt@gmail.com>
2024-08-22 22:14:19 -0300

    chore: Add Footer component and styles to Home page

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="linha"></div>
+      <a href="instagram.com" className="coloridoazul link" id="insta">Instagram</a>
+      <a href="facebook.com" className="coloridoazul link">Facebook</a>
+      <a href="whatsapp.com" className="coloridoazul link">Fale Conosco</a>
+    </footer>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,32 @@
-
+import img from "../../../public/img/image.png";
+import { Footer } from "../../components/Footer";
+import "./styles.css";
 
 export function Home() {
-  
   return (
-    <h1>
-      Página de inicio
-    </h1>
-  )
-}
+    <>
+      <img src={img} alt="Descrição da Imagem" />
 
+      <h1>
+        <span className="sombra">Transformando </span>
+        <span className="coloridoazul">espaço</span>, <br />
+        criando <span className="coloridoazul">história</span>
+      </h1>
+      <p>
+        Nós criamos ambientes <b> exclusívos </b> e <b> únicos </b>, com muito
+        bom gosto e profissionalismo, <br /> investimos tempo e
+        <span className="coloridoazul"> dedicação no seu projeto</span>. Nosso
+        compromisso vai além de simplesmente decorar.
+      </p>
+      <br />
+      <p>
+        Tenha <span className="coloridoazul">ambientes limpos</span> e bem
+        decorados, transmitindo <b>elegância</b> e <b> finesse </b>para todo
+        <br /> aquele que estiver ali. Sua história visual começa aqui!
+      </p>
+
+      <Footer />
+      
+    </>
+  );
+}
